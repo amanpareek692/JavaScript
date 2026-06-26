1f8b611143924be52e2049874485bfe59f6c31c1
amanpareek692 <coderaman123@gmail.com>
2026-06-26 18:54:19 +0530

    Added Day-01 folder

---
+++ Day_01/10.js
@@ -0,0 +1,9 @@
+let Number_1 = 23;
+let Number_2 = 98;
+if (Number_1>Number_2){
+    console.log("Number1 is Greater");
+}else if(Number_2>Number_1){
+    console.log("Number2 is Greater");
+}else{
+    console.log("Both Numbers are Equal");
+}
