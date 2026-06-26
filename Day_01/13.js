baa8893de87c18802096f94a24bcda51f0a35642
amanpareek692 <coderaman123@gmail.com>
2026-06-26 20:44:30 +0530

    Added Day-01 folder

---
+++ Day_01/13.js
@@ -0,0 +1,16 @@
+let Number1 = 97;
+let Number2 = 34;
+let Number3 = 45;
+let Number4 = 2;
+if ((Number1>Number2)&&(Number1>Number3)&&(Number1>Number4)){
+    console.log("Number1 is Largest");
+}
+else if ((Number2>Number1)&&(Number2>Number3)&&(Number2>Number4)){
+        console.log("Number2 is The Largest");
+    }
+else if((Number3>Number1)&&(Number3>Number2)&&(Number3>Number4)){
+            console.log("Number3 is Largest");
+        }
+else{
+            console.log("Number4 is Largest")
+        }
