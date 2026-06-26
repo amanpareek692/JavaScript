f010a73f7d016573968973ff551c508ce8f6dd91
amanpareek692 <coderaman123@gmail.com>
2026-06-26 22:12:27 +0530

    Added Day-01 folder

---
+++ Day_01/14.js
@@ -0,0 +1,12 @@
+let Grade = 98;
+if(Grade>=90){
+    console.log("Grade A");
+}else if(Grade>=80){
+    console.log("Grade B");
+}else if(Grade>=70){
+    console.log("Grade C");
+}else if (Grade>=60){
+    console.log("Grade D");
+}else{
+    console.log("F");
+}
