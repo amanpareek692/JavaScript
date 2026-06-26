027bc38481c87c25494671e25e2c470adc0bfd72
amanpareek692 <coderaman123@gmail.com>
2026-06-26 20:14:57 +0530

    Added Day-01 folder

---
+++ Day_01/12.js
@@ -0,0 +1,13 @@
+let Num_ber = 23;
+if(Num_ber>0){
+    console.log("The Number is Positive");
+}else if(Num_ber<0){
+    console.log("The Number is Negative");
+}else{
+    console.log("The Number is Zero");
+}
+if(Num_ber%2==0){
+    console.log("Even number");
+}else{
+    console.log(" Odd Number");
+}
